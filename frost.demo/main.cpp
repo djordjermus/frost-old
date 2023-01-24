73b4569a07fde73c70a00125c6eda65cc96ec4b1
Đorđe <djordje.rmus2000@gmail.com>
2023-01-24 21:44:55 +0100

    remove dummy.cpp from frost.system

--- frost.demo/main.cpp
+++ frost.demo/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "frost.system/_include.hpp"
 int main()
 {
+	frost::system::semaphore sf(5, 10);
+	sf.acquire();
+	sf.acquire();
+	sf.acquire();
+	sf.acquire();
+	sf.acquire();
+	//sf.acquire();
 	std::printf("Hello, %s", "World");
 }
--- frost.system/dummy.cpp
+++
@@ -1,2 +0,0 @@
-#include "_macro.hpp"
-int FROST_API dummy() { return 0; }
