2ec3b31ff502995da338152a1e61c2605a35fa7b
Đorđe <djordje.rmus2000@gmail.com>
2023-02-07 21:39:00 +0100

    implement defaultHandler

--- frost.demo/main.cpp
+++ frost.demo/main.cpp
@@ -3,18 +3,7 @@
 #include "frost.system/_include.hpp"
 frost::system::semaphore sf(0, 1);
 class handler :
-	public frost::system::gui::repositionHandler,
-	public frost::system::gui::resizeHandler,
-	public frost::system::gui::redrawHandler,
-	public frost::system::gui::cursorEnterHandler,
-	public frost::system::gui::cursorMoveHandler,
-	public frost::system::gui::cursorLeaveHandler,
-	public frost::system::gui::mouseMoveHandler,
-	public frost::system::gui::scrollWheelHandler,
-	public frost::system::gui::keyDownHandler,
-	public frost::system::gui::keyUpHandler,
-	public frost::system::gui::closeHandler,
-	public frost::system::gui::destroyHandler
+	public frost::system::gui::defaultHandler
 {
 	void handle(frost::pimpl_t<frost::system::gui> sender, frost::system::repositionGuiEvent& e) override;
 	void handle(frost::pimpl_t<frost::system::gui> sender, frost::system::resizeGuiEvent& e) override;
@@ -42,18 +31,18 @@ int main()
 
 	frost::system::gui::description desc = {};
 	auto gui = frost::system::gui(desc);
-	// gui.addHandler((frost::system::gui::repositionHandler&)h);
-	// gui.addHandler((frost::system::gui::resizeHandler&)h);
-	// gui.addHandler((frost::system::gui::redrawHandler&)h);
-	// gui.addHandler((frost::system::gui::cursorEnterHandler&)h);
-	// gui.addHandler((frost::system::gui::cursorMoveHandler&)h);
-	// gui.addHandler((frost::system::gui::cursorLeaveHandler&)h);
-	// gui.addHandler((frost::system::gui::mouseMoveHandler&)h);
+	gui.addHandler((frost::system::gui::repositionHandler&)h);
+	gui.addHandler((frost::system::gui::resizeHandler&)h);
+	gui.addHandler((frost::system::gui::redrawHandler&)h);
+	gui.addHandler((frost::system::gui::cursorEnterHandler&)h);
+	gui.addHandler((frost::system::gui::cursorMoveHandler&)h);
+	gui.addHandler((frost::system::gui::cursorLeaveHandler&)h);
+	gui.addHandler((frost::system::gui::mouseMoveHandler&)h);
 	gui.addHandler((frost::system::gui::scrollWheelHandler&)h);
 	gui.addHandler((frost::system::gui::keyDownHandler&)h);
 	gui.addHandler((frost::system::gui::keyUpHandler&)h);
-	// gui.addHandler((frost::system::gui::closeHandler&)h);
-	// gui.addHandler((frost::system::gui::destroyHandler&)h);
+	gui.addHandler((frost::system::gui::closeHandler&)h);
+	gui.addHandler((frost::system::gui::destroyHandler&)h);
 
 	while (gui.isAlive())
 	{
